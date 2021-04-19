22116572bc196f74f44eeee2c67eed73ae18911d
Giovanni <giobero4@hotmail.com>
2021-04-19 02:15:00 -0500

    Fix delay al mostrar componente SideBar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,11 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   public mbar = true;
   constructor(public router: Router) {}
+
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.toggleSidebar();
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,21 +1,32 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  AfterViewInit,
+  OnInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements AfterViewInit {
-  showSideBarMenu = false;
-  constructor(private router: Router) {}
+export class SidebarComponent implements OnInit, AfterViewInit {
+  @ViewChild('sideBarMenu')
+  sideBarMenu!: ElementRef;
+  constructor() {}
+
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.showSideBar();
   }
 
   public showSideBar(): void {
-    this.showSideBarMenu = true;
+    const $this = this;
+    setTimeout(() => {
+      $this.sideBarMenu.nativeElement.classList.add('sidebar-menu--open');
+    }, 200);
   }
 
   public navigateToSection(event: any): void {
